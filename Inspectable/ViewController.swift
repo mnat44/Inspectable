cad60d147033add465518ff1120fff22d5ce298e
mnat44 <Motoki@Motoki-no-MacBook-Pro.local>
2015-01-28 09:12:08 +0900

    Initial Commit

---
+++ Inspectable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Inspectable
+//
+//  Created by Motoki on 2015/01/28.
+//  Copyright (c) 2015年 MotokiNarita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
